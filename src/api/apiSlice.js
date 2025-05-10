61c1a949a5d123b996e4fdac22c6ba08bca6836f
KoNyaShKa <ggiziawpgg@gmail.com>
2025-05-10 11:47:37 +0300

    Tags

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -5,12 +5,23 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({//В сторе настраиваем это
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001'}),//http://localhost:3001/heroes/
+    tagTypes: ['Heroes'],//1.Обозначаем, какие метки есть в нашем api
     endpoints: builder => ({
         getHeroes: builder.query({
-            query: () => '/heroes'//Тут указано, куда будет отправляться дальнейший запрос
+            query: () => '/heroes',//Тут указано, куда будет отправляться дальнейший запрос
+            providesTags: ['Heroes']//2.Обозначили, когда данные запрашиваются при помощи query
+        }),
+        createHero: builder.mutation({
+            query: hero => ({
+                url: '/heroes',
+                method: 'POST',
+                body: hero
+            }),
+            //При использовании тегов, действия связаны последовательно 
+            invalidatesTags: ['Heroes']//3.Если данные в мутации изменились, то указываем, по какой метке можем получить актуальные данные
         })
     })
 })
 //Будет гененрироваться хук на каждое новое действие
-export const {useGetHeroesQuery} = apiSlice;//экспортируем ендпоинт, который создается выше
+export const {useGetHeroesQuery, useCreateHeroMutation} = apiSlice;//экспортируем ендпоинт, который создается выше
 //Достаем хук, который называется, как эндпоинт и приписываем тип, то есть в нашем случае запрос(query)
--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { selectAll } from '../heroesFilters/filtersSlice';
 import { heroCreated } from '../heroesList/heroesSlice';
+import { useCreateHeroMutation } from '../../api/apiSlice';
 
 import store from '../../store';
 
@@ -13,6 +14,8 @@ const HeroesAddForm = () => {
     const [heroName, setHeroName] = useState('');
     const [heroDescr, setHeroDescr] = useState('');
     const [heroElement, setHeroElement] = useState('');
+
+    const [createHero, {isLoading}] = useCreateHeroMutation();
 
     const {filtersLoadingStatus} = useSelector(state => state.filters);
     const filters = selectAll(store.getState());
@@ -32,10 +35,7 @@ const HeroesAddForm = () => {
 
         // Отправляем данные на сервер в формате JSON
         // ТОЛЬКО если запрос успешен - отправляем персонажа в store
-        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
-            .then(res => console.log(res, 'Отправка успешна'))
-            .then(dispatch(heroCreated(newHero)))
-            .catch(err => console.log(err));
+        createHero(newHero).unwrap();//Так выглядит запрос при помощи РТК и мутации, unwrap нужен, чтобы другие сущности в данном объекте отрабатывали правильно
 
         // Очищаем форму после отправки
         setHeroName('');
